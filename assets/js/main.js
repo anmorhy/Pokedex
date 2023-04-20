f63b7009026b965a39cb0a30d58052c5d3c7ccad
Anmorhy <anmorhy@gmail.com>
2023-04-20 13:41:24 -0300

    ✨

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('Sucesso!')
+console.log('Sucesso!');
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset${offset}&limit=${limit}`
+
+fetch(url)
+    // .then(function (response) {
+    //     return response.json()
+    // })
+    // .then(function (jsonBody){
+    //     console.log(jsonBody)
+    // })
+    // .catch(function (error) {
+    //     console.error(error)
+    // })
+    // .finally(function () {
+    //     console.log('Requisição Concluida')
+    // })
+
+    //ou  > o que vai pro segundo then e o retorno do primeiro
+    .then((response) => response.json()) // transforma o response em promisse do body convertido em json
+    .then((jsonBody) => console.log(jsonBody)) // recebe o body convertido e da print
+    .catch((error) => console.error(error))

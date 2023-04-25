57fc83a65d31a2e46c18fca21db95204c4282446
Anmorhy <anmorhy@gmail.com>
2023-04-25 01:15:22 -0300

    ✨

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon){
-    return `
+    return`
     <li class="pokemon">
         <span class="number">#001</span>
         <span class="name">${pokemon.name}</span>
@@ -24,54 +24,36 @@ function convertPokemonToLi(pokemon){
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMore');
 
-pokeApi.getPokemons().then((pokemons) => {
-    const newList = pokemons.map((pokemon)=> {
-        return (convertPokemonToLi(pokemon))
+
+fetch(url)
+    // .then(function (response) {
+    //     return response.json()
+    // })
+    // .then(function (jsonBody){
+    //     console.log(jsonBody)
+    // })
+    // .catch(function (error) {
+    //     console.error(error)
+    // })
+    // .finally(function () {
+    //     console.log('Requisição Concluida')
+    // })
+
+    // //ou  > o que vai pro segundo then e o retorno do primeiro
+    // .then((response) => response.json()) // transforma o response em promisse do body convertido em json
+    // .then((jsonBody) => jsonBody.results) // recebe o body convertido e da print
+    // .then((pokemonList) => console.log(pokemonList)) // recebe o body convertido e da print
+    // .catch((error) => console.error(error))
+
+    .then((response) => response.json()) // transforma o response em promisse do body convertido em json
+    .then((jsonBody) => jsonBody.results) // recebe o body convertido e da print
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            console.log(convertPokemonToLi(pokemon))
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }
+    
     })
-    const newHTML = newList.join() //convert em string com separado no ()
-    pokemonList.InnerHTML += newHTML
-
-
-    // const listItens = []
-
-    // for (let i = 0; i < pokemons.length; i++) {
-    //     const pokemon = pokemons[i];  
-    //     listItens.push(convertPokemonToLi(pokemon))
-    // }
-    // console.log(listItens)
-})
-
-
-    // const pokemonList = document.getElementById('pokemonList')
-    // pokemonList.appenChild('<li>Teste</li>') ou
-    // pokemonList.InnerHTML += '<li>Teste</li>'
-
-// fetch(url)
-//     // // .then(function (response) {
-//     // //     return response.json()
-//     // // })
-//     // // .then(function (jsonBody){
-//     // //     console.log(jsonBody)
-//     // // })
-//     // // .catch(function (error) {
-//     // //     console.error(error)
-//     // // })
-//     // // .finally(function () {
-//     // //     console.log('Requisição Concluida')
-//     // // })
-
-//     // //ou  > o que vai pro segundo then e o retorno do primeiro
-//     // .then((response) => response.json()) // transforma o response em promisse do body convertido em json
-//     // .then((jsonBody) => console.log(jsonBody)) // recebe o body convertido e da print
-//     // .catch((error) => console.error(error))
-//         
-//          .then((response) => response.json())
-//         .then((jsonBody) => jsonBody.results) 
-//         .then((pokemons) => {
-//             for (let i = 0; i < pokemons.length; i++) {
-//                 const pokemon = pokemons[i];
-//                 pokemonList.InnerHTML += convertPokemonToLi(pokemon)
-                
-//             }
-//         })
-//         .catch((error) => console.error(error))
+    .catch((error) => console.error(error))
